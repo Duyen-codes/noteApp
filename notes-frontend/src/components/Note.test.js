14c94ddebdb1f97c35787385fe29e4f48c060862
Duyen-codes <hongduyen0705@gmail.com>
2022-10-29 10:06:49 +0300

    wrote test for rendering el content with render and screen method

--- notes-frontend/src/components/Note.test.js
+++ notes-frontend/src/components/Note.test.js
@@ -1,9 +1,11 @@
+// import dependencies
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
+// import react-testing methods
 import { render, screen } from "@testing-library/react";
 import Note from "./Note";
 
-test("renders content", () => {
+test.only("renders content", () => {
   const note = {
     content: "Component testing is done with react-testing-library",
     important: true,
@@ -14,5 +16,20 @@ test("renders content", () => {
   const element = screen.getByText(
     "Component testing is done with react-testing-library"
   );
+  screen.debug(element);
   expect(element).toBeDefined();
+});
+
+test("renders content with container and querySelector", () => {
+  const note = {
+    content: "Component testing is done with react-testing-library",
+    important: true,
+  };
+
+  const { container } = render(<Note note={note} />);
+
+  const div = container.querySelector(".note");
+  expect(div).toHaveTextContent(
+    "Component testing is done with react-testing-library"
+  );
 });
