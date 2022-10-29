14cc6de74944d8743458ac6e1fc9355cef821edd
Duyen-codes <hongduyen0705@gmail.com>
2022-10-29 16:24:42 +0300

    tested new note form

--- notes-frontend/cypress/e2e/note_app.cy.js
+++ notes-frontend/cypress/e2e/note_app.cy.js
@@ -19,4 +19,22 @@ describe("Note app", function () {
     // this row ensures taht login was successful
     cy.contains("Welcome Matti Luukkainen. You are now logged-in");
   });
+
+  // Testing new note form
+  describe("when logged in", function () {
+    // only logged-in users can create new notess, so logging in to the app added to a beforeEach block
+    beforeEach(function () {
+      cy.get('a[href*="login"]').click();
+      cy.get("#username").type("mluukkai");
+      cy.get("#password").type("salainen");
+      cy.get("#login-button").click();
+    });
+
+    it("a new note can be created", function () {
+      cy.contains("new note").click();
+      cy.get("input").type("a note created by cypress");
+      cy.contains("save").click();
+      cy.contains("a note created by cypress");
+    });
+  });
 });
